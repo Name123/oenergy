a63b5fcd317b02bf655a212b9fd3fec6ca6f5963
Leonid Podolsky <podolsky_la@yahoo.com>
2023-02-01 19:14:09 +0100

    Add some tests

--- d0010/admin.py
+++ d0010/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from d0010.models import DataFile, RegisterReading
 
-# Register your models here.
+
+class RegisterReadingAdmin(admin.ModelAdmin):
+    model = RegisterReading
+    list_display = ('datetime', 'value', 'meter_register_id', 'filename', 'mpan_core', 'meter_serial_num')
+    list_filter = ('mpan__mpan_core', 'meter__meter_serial_num')
+
+    def filename(self, obj):
+        return obj.data_file.filename
+
+    def mpan_core(self, obj):
+        return obj.mpan.mpan_core
+
+    def meter_serial_num(self, obj):
+        return obj.meter.meter_serial_num
+
+
+class DataFileAdmin(admin.ModelAdmin):
+    model = DataFile
+    list_display = ('filename', 'loaded_at')
+
+
+admin.site.register(RegisterReading, RegisterReadingAdmin)
+admin.site.register(DataFile, DataFileAdmin)
--- d0010/tests.py
+++ d0010/tests.py
@@ -1,3 +1,50 @@
+from datetime import datetime
+
+
 from django.test import TestCase
 
-# Create your tests here.
+
+from d0010.models import DataFile, MeterData, MpanData, RegisterReading
+from d0010.parse import FlowD0010Parser
+
+
+class ParserTest(TestCase):
+    def setUp(self):
+        self._parser = FlowD0010Parser()
+        self._parser._curr_file = DataFile(filename='test.txt')
+        self._parser._curr_file.save()
+
+    def test_header_parsing(self) -> None:
+        self.assertFalse(self._parser._is_header_read)
+        self._parser._parse_line('ZHV|')
+        self.assertTrue(self._parser._is_header_read)
+
+    def test_footer_parsing(self) -> None:
+        self.assertFalse(self._parser._is_footer_read)
+        self._parser._parse_line('ZPT|')
+        self.assertTrue(self._parser._is_footer_read)
+
+    def test_mpan_parsing(self) -> None:
+        self.assertIsNone(self._parser._curr_mpan)
+        self._parser._parse_line('ZHV|')
+        self.assertEqual(MpanData.objects.count(), 0)
+        self._parser._parse_line('026|12345|V|')
+        self.assertEqual(MpanData.objects.count(), 1)
+        obj = MpanData.objects.first()
+        self.assertEqual(obj.mpan_core, 12345)
+        self.assertEqual(obj.validation_status, 'V')
+        
+
+    def test_reading_parsing(self) -> None:
+        self._parser._parse_line('ZHV|')
+        self._parser._parse_line('026|12345|V|')
+        self._parser._parse_line('028|ABC 00|D|')
+        self._parser._parse_line('030|01|20220101000000|100.0||||N|')
+        self._parser._parse_line('ZPT|')
+        mpan = MpanData.objects.first()
+        meter = MeterData.objects.first()
+        reading = RegisterReading.objects.first()
+        self.assertEqual(reading.mpan, mpan)
+        self.assertEqual(reading.meter, meter)
+        self.assertEqual(int(reading.value), 100)
+        self.assertEqual(reading.meter_register_id, '01')
